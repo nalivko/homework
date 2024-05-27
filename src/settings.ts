f42da8e4e0916cf259f2c9ffdc48f5e755da9b42
Dmitriy <nalivko.dmitr@gmail.com>
2024-05-27 10:58:56 +0300

    change port number in settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config() // добавление переменных из файла .env в process.env
 
 export const SETTINGS = {
-    PORT: process.env.PORT || 3003,
+    PORT: process.env.PORT || 5000,
     PATH: {
         VIDEOS: "/api/videos",
     }
